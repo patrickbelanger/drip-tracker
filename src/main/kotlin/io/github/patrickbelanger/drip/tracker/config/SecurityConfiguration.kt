41ac45342e73986300f76a7b7cdfaa244a4edb6b
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2024-02-13 23:44:52 -0500

    chore: enabling h2 console

--- src/main/kotlin/io/github/patrickbelanger/drip/tracker/config/SecurityConfiguration.kt
+++ src/main/kotlin/io/github/patrickbelanger/drip/tracker/config/SecurityConfiguration.kt
@@ -2,12 +2,15 @@ package io.github.patrickbelanger.drip.tracker.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-
+import javax.sql.DataSource
 
 @Configuration
 @EnableWebSecurity
@@ -15,26 +18,46 @@ class SecurityConfiguration {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
+      return http
             .authorizeHttpRequests { authorize ->
                 authorize
-                    .requestMatchers("/welcome").permitAll()
+                    .requestMatchers(
+                            "/login*",
+                            "/error",
+                            "/h2-console/**").permitAll()
                     .anyRequest().authenticated()
-
             }
             .formLogin { authorize ->
                 authorize
-                    .loginPage("/login").permitAll()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
             }
             .logout { logout ->
                 logout
-                    .logoutSuccessUrl("/welcome")
+                    .logoutSuccessUrl("/login") // Temporary
             }
-            .sessionManagement { session ->
-                session
-                    .maximumSessions(1)
+            .cors { cors ->
+                cors
+                    .disable()
             }
-        return http.build()
+            .csrf { csrf ->
+                csrf
+                    .ignoringRequestMatchers("/h2-console/**")
+            }
+            .headers { headers ->
+                headers.frameOptions { frameOptionsCustomizer ->
+                    frameOptionsCustomizer.disable()
+                }
+            }
+            .build()
+    }
+
+    @Bean
+    fun dataSource(): DataSource {
+        return EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build()
     }
 
     @Bean
